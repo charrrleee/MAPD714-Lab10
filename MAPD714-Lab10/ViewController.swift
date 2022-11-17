ad09e26c8fdb4c6c5d7c820b56e2e5eacdaf984a
charrrleee <charlcpy@gmail.com>
2022-11-16 20:14:20 -0500

    try first persistence storage (file)

--- MAPD714-Lab10/ViewController.swift
+++ MAPD714-Lab10/ViewController.swift
@@ -7,13 +7,60 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController
+{
+    @IBOutlet var lineFields: [UITextField]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        let fileURL = self.dataFileURL()
+
+        if (FileManager.default.fileExists(atPath: fileURL.path!)) {
+            if let array = NSArray(contentsOf: fileURL as URL) as? [String] {
+                for i in 0..<array.count {
+                    lineFields[i].text = array[i]
+                }
+            }
+                        
+         }
+
+        let app = UIApplication.shared
+        NotificationCenter.default.addObserver(
+            self,selector: #selector(self.applicationWillResignActive(notification:)),
+                                               name: UIScene
+            .willDeactivateNotification, object: nil)
+    }
+    
+    func dataFileURL() -> NSURL
+    {
+        let urls = FileManager.default.urls(
+            for: .documentDirectory,
+            in: .userDomainMask)
+        var url: NSURL?
+        url = URL(fileURLWithPath: "") as NSURL?
+        do
+        {
+            url = urls.first?.appendingPathExtension("data.plist") as NSURL?
+        }
+        catch
+        {
+            print("Error is \(error)")
+        }
+        return url!
     }
 
+    @objc func applicationWillResignActive(notification: NSNotification) {
+         let fileURL = self.dataFileURL()
+        print("in application will resign active")
+//         let array = (self.lineFields as NSArray).value(forKey: "text") as! NSArray
+        let array: NSMutableArray = []
+        for line in self.lineFields
+        {
+            array.add(line.text!)
+        }
+         array.write(to: fileURL as URL, atomically: true)
+        }
 
 }
 
